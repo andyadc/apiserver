07bd6e7c23ebdec796319c33833627e401e85be3
99BILL\andy.an <andy.an@99bill.com>
2018-07-02 19:47:31 +0800

    Initial project

---
+++ Makefile
@@ -0,0 +1,2 @@
+all: gotool
+	@go build -v .
---
+++ admin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SERVER="apiserver"
+BASE_DIR=$PWD
+INTERVAL=2
+
+# 命令行参数，需要手动指定
+ARGS=""
+
+function status() {
+if [ "`pgrep $SERVER -u $UID`" != "" ];then
+		echo $SERVER is running
+	else
+		echo $SERVER is not running
+	fi
+}
