3eea3e30d9671d6d48720a2ab6f0523546ee32f9
99BILL\andy.an <andy.an@99bill.com>
2018-07-02 16:35:04 +0800

    Initial project

--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
 import (
+	"apiserver/config"
 	"apiserver/router"
 	"errors"
 	"github.com/gin-gonic/gin"
-	"log"
+	logger "github.com/lexkong/log"
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 	"net/http"
 	"time"
 )
 
+var (
+	cfg = pflag.StringP("config", "c", "", "apiserver config file path.")
+)
+
 func main() {
+
+	pflag.Parse()
+
+	if err := config.Init(*cfg); err != nil {
+		panic(err)
+	}
+
+	// Set gin mode.
+	gin.SetMode(viper.GetString("runmode"))
+
 	// Create the Gin engine.
 	g := gin.New()
 
@@ -27,26 +44,26 @@ func main() {
 	// Ping the server to make sure the router is working.
 	go func() {
 		if err := pingServer(); err != nil {
-			log.Fatal("The router has no response, or it might took too long to start up.", err)
+			logger.Fatal("The router has no response, or it might took too long to start up.", err)
 		}
-		log.Print("The router has been deployed successfully.")
+		logger.Info("The router has been deployed successfully.")
 	}()
 
-	log.Printf("Start to listening the incoming requests on http address: %s", ":8080")
-	log.Println(http.ListenAndServe(":8080", g).Error())
+	logger.Infof("Start to listening the incoming requests on http address: %s", viper.GetString("port"))
+	logger.Info(http.ListenAndServe(viper.GetString("port"), g).Error())
 }
 
 // pingServer pings the http server to make sure the router is working.
 func pingServer() error {
 	for i := 0; i < 2; i++ {
 		// Ping the server by sending a GET request to `/health`.
-		resp, err := http.Get("http://127.0.0.1:8080" + "/sd/health")
+		resp, err := http.Get(viper.GetString("serverurl") + "/sd/health")
 		if err == nil && resp.StatusCode == 200 {
 			return nil
 		}
 
 		// Sleep for a second to continue the next ping.
-		log.Print("Waiting for the router, retry in 1 second.")
+		logger.Info("Waiting for the router, retry in 1 second.")
 		time.Sleep(time.Second)
 	}
 
