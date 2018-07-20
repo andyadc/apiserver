e15af1cac18bd13abf0bd9ee7ba307caef05746d
99BILL\andy.an <andy.an@99bill.com>
2018-07-20 17:05:03 +0800

    api server

---
+++ model/user.go
@@ -0,0 +1,36 @@
+package model
+
+// User represents a registered user.
+type UserModel struct {
+	BaseModel
+	Username string `json:"username" gorm:"column:username;not null" binding:"required" validate:"min=1,max=32"`
+	Password string `json:"password" gorm:"column:password;not null" binding:"required" validate:"min=5,max=128"`
+}
+
+func (u *UserModel) TableName() string {
+	return "tb_users"
+}
+
+// Create creates a new user account.
+func (u *UserModel) Create() error {
+	return DB.Self.Create(&u).Error
+}
+
+// DeleteUser deletes the user by the user identifier.
+func Delete(id uint64) error {
+	user := UserModel{}
+	user.Id = id
+	return DB.Self.Delete(&user).Error
+}
+
+// Update updates an user account information.
+func (u *UserModel) Update() error {
+	return DB.Self.Save(&u).Error
+}
+
+// GetUser gets an user by the user identifier.
+func GetUser(username string) (*UserModel, error) {
+	u := &UserModel{}
+	d := DB.Self.Where("username = ?", username).First(&u)
+	return u, d.Error
+}
---
+++ pkg/auth/auth.go
@@ -0,0 +1,14 @@
+package auth
+
+import "golang.org/x/crypto/bcrypt"
+
+// Encrypt encrypts the plain text with bcrypt.
+func Encrypt(s string) (string, error) {
+	hashedBytes, err := bcrypt.GenerateFromPassword([]byte(s), bcrypt.DefaultCost)
+	return string(hashedBytes), err
+}
+
+// Compare compares the encrypted text with the plain text if it's the same.
+func Compare(hashedPassword, password string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
